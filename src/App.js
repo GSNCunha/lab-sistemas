15d2773d94041ea07a529590bd4058f6fa08d8ba
code <code@Gabriels-Mac-mini.local>
2023-01-17 17:20:06 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1>agora sou eu fazendo algo aqui</h1>
+    </div>
+  );
+}
+
+export default App;
