bc25c6ef0da333fa71b77c8011c348703d7dff61
code <code@Gabriels-Mac-mini.local>
2023-01-21 13:53:29 -0300

    led hooks working properly

--- src/Components/LED.js
+++ src/Components/LED.js
@@ -4,12 +4,50 @@ const LED = (props) => {
     const [counter, setCounter] = useState(0);
 
     const increment = () => {
-            setCounter(counter+1);   
+        if(counter < 100)
+        {
+            setCounter(counter+25);  
+        }else 
+        {
+            setCounter(0);  
+        }
+             
     }; 
-    return(
-        <div className='LedSquare' onClick={increment}>
-            {counter}
-       </div>
+    if(counter == 0)
+    {
+        return(
+            <div className='LedSquare ledOff' onClick={increment}>
+                {counter}
+        </div>
     );
+    }else if(counter == 25)
+    {
+        return(
+            <div className='LedSquare ledOn25' onClick={increment}>
+                {counter}
+           </div>
+        );  
+    }else if(counter == 50)
+    {
+        return(
+            <div className='LedSquare ledOn50' onClick={increment}>
+                {counter}
+           </div>
+        );  
+    }else if(counter == 75)
+    {
+        return(
+            <div className='LedSquare ledOn75' onClick={increment}>
+                {counter}
+           </div>
+        );  
+    }else
+    {
+        return(
+            <div className='LedSquare ledOn100' onClick={increment}>
+                {counter}
+           </div>
+        );  
+    }
 };
 export default LED;
