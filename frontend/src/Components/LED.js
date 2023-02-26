6429a6f50c1b22c64d254614a5aa8c5692fa7d0a
code <code@Gabriels-Mac-mini.local>
2023-02-26 15:12:53 -0300

    fetching data every second

--- frontend/src/Components/LED.js
+++ frontend/src/Components/LED.js
@@ -19,6 +19,7 @@ export const EDIT_CLIENT = gql`
   }
 `;
 
+
 const LED = (props) => {
 
     const [editClient, editClientInfo] = useMutation(EDIT_CLIENT);
@@ -36,19 +37,28 @@ const LED = (props) => {
 
 
     const [counter, setCounter] = useState(0);
-    const { data, loading, error } = useQuery(GET_CLIENTS);
+    const { data, loading, error, refetch } = useQuery(GET_CLIENTS);
   
     useEffect(() => {
+        const intervalId = setInterval(() => {
+            refetch();
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [refetch]);
+
+    useEffect(() => {
       if (data && data.clients) {
-        setCounter(parseInt(data.clients[props.number].state), 2);
+        setCounter(parseInt(data.clients[props.number].state, 10));
       }
     }, [data]);
 
     const increment = () => {
-        if(counter < 100)
+        if(counter < 99)
         { 
-            handleEdit(data.clients[props.number].id,(counter+25).toString());
-            setCounter(counter+25); 
+            handleEdit(data.clients[props.number].id,(counter+33).toString());
+            setCounter(counter+33); 
+
         }else 
         {
             handleEdit(data.clients[props.number].id, "0")
@@ -56,28 +66,22 @@ const LED = (props) => {
         }
              
     }; 
-    if(counter == 0)
+
+    if(counter === 0)
     {
         return(
             <div className='LedSquare ledOff' onClick={increment}>
                 
         </div>
     );
-    }else if(counter == 25)
+    }else if(counter === 33)
     {
         return(
             <div className='LedSquare ledOn25' onClick={increment}>
                
            </div>
         );  
-    }else if(counter == 50)
-    {
-        return(
-            <div className='LedSquare ledOn50' onClick={increment}>
-               
-           </div>
-        );  
-    }else if(counter == 75)
+    }else if(counter === 66)
     {
         return(
             <div className='LedSquare ledOn75' onClick={increment}>
@@ -93,4 +97,5 @@ const LED = (props) => {
         );  
     }
 };
+
 export default LED;
