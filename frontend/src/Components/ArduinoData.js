f9eefd547c90096d60b6e150b482e92abd0b1cdb
code <code@MacBook-Air-de-Gabriel.local>
2023-03-21 09:56:10 -0300

    everything working

--- frontend/src/Components/ArduinoData.js
+++ frontend/src/Components/ArduinoData.js
@@ -1,6 +1,6 @@
 export function NumberConverter(value, led) {
     const binaryString = toBinaryString(value);
-    const start = (3-led) * 2; // calculate starting index of substring
+    const start = (led) * 2; // calculate starting index of substring
     const binarySubstring = binaryString.substr(start, 2); // get two-digit substring
     const decimalValue = parseInt(binarySubstring, 2); // convert substring to decimal
     return decimalValue;
